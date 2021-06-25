a13d519cb8a90b5df162fbd32736222dbaaf052d
ShopITSupport <micromadnesspvtltd@gmail.com>
2021-06-25 11:17:01 +0530

    Initial Commit

---
+++ BACKEND/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+app.use(express.json());
+
+/* http://localhost:3000/api/v1/products */
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair_dresser',
+        image: 'some_url'
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    res.send(newProduct);
+})
+
+app.listen(3000, () => {
+    console.log('Server is running now on port 3000');
+})
