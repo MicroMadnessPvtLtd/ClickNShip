469791e587edf538959d2d1088a114c3899e935c
ShopITSupport <micromadnesspvtltd@gmail.com>
2021-06-26 07:53:16 +0530

    Commit Initial Project Structure

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
-/* http://localhost:3000/api/v1/products */
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair_dresser',
-        image: 'some_url'
-    }
-    res.send(product);
-})
+const productsRoutes = require('./routers/products');
+const categoriesRoutes = require('./routers/categories');
+const ordersRoutes = require('./routers/orders');
+const usersRoutes = require('./routers/users');
 
-app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    res.send(newProduct);
-})
+
+/* Routers */
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/users`, usersRoutes);
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'CnSDev0'
+}).then(() => {
+    console.log('Database connection established');
+}).catch((err) => {
+    console.log(err);
+});
 
 app.listen(3000, () => {
     console.log('Server is running now on port 3000');
