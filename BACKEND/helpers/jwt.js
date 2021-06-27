3cf7891ddefe697a450a4ea27dc562c6472b6783
ShopITSupport <micromadnesspvtltd@gmail.com>
2021-06-27 19:34:03 +0530

    Backend Section Completed

--- BACKEND/helpers/jwt.js
+++ BACKEND/helpers/jwt.js
@@ -12,7 +12,8 @@ function authJwt() {
             `${api}/authenticate/login`,
             `${api}/authenticate/register`,
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] }
+            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
         ]
     })
 }
