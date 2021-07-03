97335473496ec58252dfbab570d1d46ca44bbb40
ShopITSupport <micromadnesspvtltd@gmail.com>
2021-07-03 10:24:57 +0530

    Category Front End Admin

--- micro-madness/apps/admin/src/app/app.module.ts
+++ micro-madness/apps/admin/src/app/app.module.ts
@@ -1,11 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ColorPickerModule } from 'primeng/colorpicker';
+
+import { MessageService } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
+
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
+
+const UX_MODULE = [
+    CardModule,
+    ToolbarModule,
+    ButtonModule,
+    TableModule,
+    InputTextModule,
+    ToastModule,
+    ConfirmDialogModule,
+    ColorPickerModule
+]
 
 const routes: Routes = [
     {
@@ -15,15 +44,35 @@ const routes: Routes = [
             {
                 path: 'dashboard',
                 component: DashboardComponent
+            },
+            {
+                path: 'categories',
+                component: CategoriesListComponent
+            },
+            {
+                path: 'categories/form',
+                component: CategoriesFormComponent
+            },
+            {
+                path: 'categories/form/:id',
+                component: CategoriesFormComponent
             }
         ]
     }
 ]
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
-    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
-    providers: [],
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent],
+    imports: [
+        BrowserModule, 
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes, { initialNavigation: 'enabled' }), 
+        HttpClientModule,
+        BrowserAnimationsModule,
+        ...UX_MODULE
+    ],
+    providers: [MessageService, ConfirmationService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
